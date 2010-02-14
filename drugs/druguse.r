7c7130407a7f6eee23c5b2b92a82cd92f05c26c3
Diego Valle Jones <diegovalle@gmail.com>
2010-02-14 11:49:07 -0600

    Regressions on drug use and the homicide rate

---
+++ drugs/druguse.r
@@ -0,0 +1,47 @@
+########################################################
+#####       Author: Diego Valle Jones
+#####       Website: www.diegovalle.net
+#####       Date Created: Fri Feb 12 21:48:28 2010
+########################################################
+#Regressions of drug use and homicides rates to see if
+#Castañeda and Aguilar are right that an increase in drug use
+#hasn't increased violence
+
+library(ggplot2)
+library(car)
+
+drugs <- read.csv("data/druguse.csv")
+#The drug usage as a percetage
+drugs[,2:7] <- drugs[,2:7] / 100
+
+drawPlots <- function(df, x, y) {
+  reg <- lm(logit(drugs[[x]], percents = TRUE) ~ drugs[[y]])
+  pvalue <- summary(reg)$coefficients[,4][2]
+  color <- ifelse(pvalue > .05, "red", "blue")
+  if(pvalue > .05 & pvalue < .10)
+    color <- "black"
+  ggplot(df, aes_string(x = x, y = y, label = "Abbrv")) +
+         geom_text(alpha = .5) +
+         stat_smooth(method = lm, color = color) +
+         #let's make the axes look the same
+         coord_cartesian(ylim = c(-9, 80)) +
+         scale_x_continuous(formatter = "percent") +
+         opts(legend.position = "none")
+}
+
+grid.newpage()
+pushViewport(viewport(layout =  grid.layout(nrow = 2, ncol = 2)))
+
+subplot <- function(x, y) viewport(layout.pos.row = x,
+                                   layout.pos.col = y)
+
+print(drawPlots(drugs, "Cocaine", "Homicide.Rate"),
+      vp = subplot(1, 1))
+print(drawPlots(drugs, "Marihuana", "Homicide.Rate"),
+      vp = subplot(1, 2))
+print(drawPlots(drugs, "Amphetamines", "Homicide.Rate"),
+      vp = subplot(2, 1))
+print(drawPlots(drugs, "Illegal.Drugs", "Homicide.Rate"),
+      vp = subplot(2, 2))
+
+dev.print(png, "output/Drug Use.png", width=500, height=500)
---
+++ drugs/readme.md
@@ -0,0 +1,27 @@
+Drug Use and its Relation to the Drug War
+=========================================
+Regressions on drug use and the homicide rate to see if they are correlated. A blue line means the p-value < 0.5 and a red line that it is not significant.
+
+Sources
+------
+
+[Encuesta Nacional de Addicciones 2008](http://www.insp.mx/medios/noticias/index.php?art/id:263):
+
+*   Any Drug,	Illegal Drugs,	and Medical Drugs:
+
+    Cuadro A47
+
+    Incidencia acumulada para el uso de drogas por entidad.
+
+    Población total de 12 a 65 años. México, ENA 2008
+
+*   Mariguana,	Cocaine, and	Amphetamines:
+
+    Cuadro A53
+
+    Incidencia acumulada para el uso de drogas por entidad.
+
+    Población total de 12 a 65 años. México, ENA 2008
+
+    
+[State Abbreviations](http://www.statoids.com/umx.html): Which for some reason abbreviates Baja California as BCN, there's no Norte in California! I changed it to BC.
