1a7e18a4f207ac1fdd29c42a23c14e2a7e6937ea
Diego Valle Jones <diegovalle@gmail.com>
2010-02-17 11:31:16 -0600

    updated data from the Statistical Yearbooks

--- missing-homicides/icesi-vs-inegi.r
+++ missing-homicides/icesi-vs-inegi.r
@@ -7,16 +7,22 @@
 #homicide data in Mexico
 #2. Scatter plot of the INEGI homicide data vs the ICESI.
 #Chihuahua is a big outlier
+#3. Bar plot of the differences
+#4. Scatter plot against the proportions
 
 library(ggplot2)
 library(directlabels)
 
+
+########################################################
+# Line plot of PAHO, UN, INEGI and ICESI homicide rates
+########################################################
 homts <- read.csv("data/PAHO-UN-INEGI-ICESI.csv")
 ggplot(melt(homts, id="Year"), aes(Year, value, group = variable,
                                    color = variable)) +
        geom_line(size=1.5) +
        ylab("Homicide rate") +
-      opts(title = "Different estimates of the homicide rate")
+      opts(title = "Different estimates of the ‘homicide’ rate")
 dev.print(png, file = "output/PAHO-UN-INEGI-ICESI.png", width = 500, height = 300)
 
 #FADE IN:
@@ -76,14 +82,105 @@ dev.print(png, file = "output/PAHO-UN-INEGI-ICESI.png", width = 500, height = 30
 #                                                           FADE OUT
 
 #                  THE END
+
+
+########################################################
+# Scatter Plot
+########################################################
+
 ivsi <- read.csv("data/INEGIvsICESI.csv")
+#Get rid of the full name of the states (eg: Veracruz de
+#Ignacio de la Llave changes to Veracruz
+cleanNames <- function(df, varname = "County"){
+  df[[varname]] <- gsub("* de .*","", df[[varname]])
+  df[[varname]]
+}
+
+ivsi$State <- cleanNames(ivsi, "State")
+
 ggplot(ivsi, aes(INEGI, ICESI,
                  label = paste(State," (",
                        as.character(INEGI-ICESI), ")", sep = ""))) +
        geom_text(aes(size = sqrt(abs(INEGI-ICESI))), hjust=-.1) +
        geom_point() +
        geom_abline(slope=1, linetype=2, color="blue") +
-       opts(title = "Who's hiding homicides? (INEGI - ICESI)") +
+       opts(title = "Differences in ‘homicide’ reporting rates (INEGI - ICESI)") +
        scale_x_continuous(limits = c(0, 4000)) +
-       opts(legend.position = "none")
+       opts(legend.position = "none") +
+       annotate("text", 1400, 1400, label = "Data ara equal ->",
+                color ="blue", hjust = 1)
 dev.print(png, file = "output/INEGIvsICES.png", width = 600, height = 480)
+
+
+
+
+########################################################
+# Bar Plot
+########################################################
+drawBars <- function(df) {
+  values <- cast(df)
+  labels <- data.frame(State = values$State,
+                       variable = c("INEGI"),
+                       value = ifelse(values$INEGI > ivsi[ ,3],
+                                      values$INEGI,
+                                      values[,3]),
+                       missing = values[,2] - values[,3])
+  labels$missing <- ifelse(labels$missing < 0,
+                         as.character(labels$missing),
+                         as.character(paste("+",
+                                            labels$missing, sep="")))
+  df$State <- with(df, reorder(factor(State), value))
+  ggplot(df, aes(x=State, y=value, group = variable,
+                    fill = variable)) +
+         opts(title = "Differences in reported ‘homicides’ (2008)") +
+         geom_bar(stat = "identity", position = "identity",
+                  alpha = .5) +
+         ylab("Number of Homicides") +
+         geom_text(data = labels, aes(label = missing), hjust=-.1,
+                   color = "gray40") +
+         scale_y_continuous(limits = c(0, 3000)) +
+         coord_flip()
+}
+#ICESI data
+drawBars(melt(ivsi[ , c(1:2,3)], id="State"))
+dev.print(png, file = "output/INEGIvsICESI.png", width = 480, height = 600)
+
+#data from the statistical yearbooks
+drawBars(melt(ivsi[ , c(1:2,6)], id="State"))
+dev.print(png, file = "output/INEGIvsYearbook.png", width = 480, height = 600)
+
+
+########################################################
+# Percentage Difference
+########################################################
+ggplot(ivsi, aes(INEGI, abs(INEGI - Stat.Yrbks) /
+                 (Stat.Yrbks + INEGI) / 2, label = Abbrv)) +
+    geom_text(alpha = .6)+
+    #Not significant
+    geom_smooth(method=lm, se = FALSE, color ="red") +
+    scale_y_continuous(formatter = "percent") +
+    xlab("Number of Homicides according to INEGI") +
+    ylab("Percentage difference")
+dev.print(png, file = "output/INEGIvsICESIprop.png", width = 480, height = 480)
+
+with(ivsi, {
+     dep <- abs((INEGI - Stat.Yrbks) /
+                 ((Stat.Yrbks + INEGI) / 2)  )
+     summary(lm(INEGI ~ dep, data = ivsi))
+ })
+
+
+
+########################################################
+#Dot plot
+########################################################
+
+mivsi <- melt(ivsi[ , c(1:3,6)], id="State")
+mivsi$State <- with(mivsi, reorder(factor(State),value))
+ggplot(mivsi, aes(value, State, group = variable,
+                  fill = variable)) +
+       opts(title = "Differences in reported ‘homicides’") +
+       xlab("Number of Homicides") +
+       geom_point(aes(color = variable, shape = variable),
+                  size = 3, alpha = .5)
+dev.print(png, file = "output/INEGIvsICESIvsYear.png", width = 480, height = 600)
--- missing-homicides/readme.md
+++ missing-homicides/readme.md
@@ -1,17 +1,40 @@
-﻿How realiable are Mexican Homicide Statistics
+﻿How reliable are Mexican Homicide Statistics?
 ================================================
 
 There are 2 main sources of homicide statistics in Mexico, the vital
-statistics from the INEGI and the police data which are available at
+statistics from the INEGI, and the police data which are available at
 the ICESI (a civic association not affiliate with the
-government). Since both are available at the state level you can
+government which gets the data from the SNSP [Secretaría Nacional de Seguridad Pública]). But as it turns out the data from the ICESI is only an estimate
+(even if the secretary of the interior uses the data in his presentations
+as if it were the number of homicides), 
+still, you can download the final numbers from the Statistical Yearbooks of 
+each state. Since both data sources are available at the state level you can
 compare them and see if there are any differences.
-Data are also available from PAHO and the UN, but only at the national level, so I also compared them.
+
+The vital statistics from the INEGI, follow the [International Classification of Diseases](http://www.who.int/classifications/icd/en/) and so it is valid to do international comparisons with them.
+
+The plot gets thicker: it turns out the data from the ICESI for 2008 are only
+estimates (missing over 1,100 homicides in Chihuahua, looks like it was a mid-year
+"estimate"). The INEGI releases a series of statistical yearkbooks for each state
+that contain the final (final being over 90% reported) data according to the local police forces (homicide is a 
+local crime in Mexico). I didn't include the statistical yearbooks because they're
+about 250MB, but in the directory "anuarios" you'll find a script to download them
+
+Even thicker: the data from the ICESI and the statistical yearbooks are not even 
+homicides, but police reports. So if 18 kids are killed in a massacre at the same 
+time, and there's only one police report, they are recorded as one. The statistical
+yearbooks do have the data available by number of victims, but they combine "homicidios
+dolosos" (homicide) with "homicidios culposos" (manslaughter). I wonder, wonder why
+they would do this.
+
+Data are also available from PAHO and the UN, but only at the national level.
 
 Output
 ------
-* Chart of homicides rate according to the INEGI, ICESI, PAHO, and the UN Crime Survey
+* Chart of homicides rate according to the INEGI, ICESI, PAHO, and the UN Crime Survey.
+* Bar plot of the differences in number if homicides, one for the ICESI data and another for the final data from the Statistical Yearbooks.
 * Scatter plot of the different homicide data (INEGI vs ICESI). That is see if the police records (labeled ICESI) match those of the vital statistics system(labeled INEGI). The police records are missing over 1,100 homicides in Chihuahua alone.
+* Scatter plot against the proportions.
 
 Sources
 ------
@@ -21,10 +44,30 @@ __Mexican Homicide Data:__
 
 [ICESI](http://www.icesi.org.mx/documentos/estadisticas/estadisticas/denuncias_homicidio_doloso_1997_2008.xls)
 
-[PAHO] (http://www.paho.org/English/SHA/coredata/tabulator/newTabulator.htm) Pan American Health Organization, Health Analysis and Statistics Unit. Regional Core Health Data Initiative; Technical Health Information System. Washington DC, 2007.
+[Statistical Yearbooks](http://www.inegi.org.mx/est/contenidos/espanol/sistemas/sisnav/selproy.aspx): The following states were not available as of Feb-17-2009, so I used the estimates that appear in the file from the ICESI: Durango, Tlaxcala, Yucatan and Nayarit. Furthermore there was a mistake in the statistical yearbook for Querétaro (the data from "homicidios dolosos"[homicide] and "homicidios culposos"[manslaughter] was transposed).
+
+[PAHO](http://www.paho.org/English/SHA/coredata/tabulator/newTabulator.htm) Pan American Health Organization, Health Analysis and Statistics Unit. Regional Core Health Data Initiative; Technical Health Information System. Washington DC, 2007.
 
 UN Crime Survey: Eight and Seventh United Nations Survey of Crime Trends and Operations of Criminal Justice Systems
 
 __Population Data:__
 
 [Indicadores demográficos básicos 1990-2030](http://www.conapo.gob.mx/index.php?option=com_content&view=article&id=125&Itemid=203)
+
+CodeBook:
+---------
+INEGIvsICESI.csv
+
+Colums:
+State	| INEGI	| ICESI	| Abbrv	| Anuario	| Stat.Yrbks
+
+State - Name of the State
+
+INEGI - Number of homicides according to the INEGI
+
+ICESI - Number of homicides according to the ICESI (which takes the data from SNSP)
+
+Anuario - The raw values according to the Statistical Yearbooks
+
+Stat.Yrbks - The values from the Statistical Yearbook if available, if not, the data is from the ICESI
+
